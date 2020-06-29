054d3b44901e4ff0f1733ed1de05c449762201bf
Marc <mlanglois333@icloud.com>
2020-06-29 16:09:57 -0400

    Linked confirm boxes to var final

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 // Assignment Code
 
 var generateBtn = document.querySelector("#generate");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var number = "1234567890";
+var special = "!@#$%^&*()";
+var final = " "
 
 function selectInput() {
 var charSelect = Number(prompt("Select number of charcters (between 8 and 128)"));
-console.log(charSelect);
-var upSelect = confirm("Does the password require uppercase letters?");
-var lowSelect = confirm("Does the password require lowercase letters?");
-var numSelect = confirm("Does the password require numbers?");
-var specSelect = confirm("Does the password require special characters?");
 
-var uppercase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-var lowercase = ['abcdefghijklmnopqrstuvwxyz'];
-var number = ['1234567890'];
-var special = ['!@#$%^&*()'];
+
+var upSelect = confirm("Does the password require uppercase letters?");
+if  (upSelect === true) 
+{var final = final + uppercase};
+console.log(final);
+
+var lowSelect = confirm("Does the password require lowercase letters?");
+if  (lowSelect === true) 
+{var final = final + lowercase};
+console.log(final);
+
+var numSelect = confirm("Does the password require numbers?");
+if  (numSelect === true) 
+{var final = final + number};
+console.log(final);
+
+var specSelect = confirm("Does the password require special characters?");
+if  (specSelect === true) 
+{var final = final + special};
+console.log(final);
+
+
 }
 
 
