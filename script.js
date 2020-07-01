deec91ce72fe9b4b52678a74838151f1eb05fb98
Marc <mlanglois333@icloud.com>
2020-07-01 12:21:12 -0400

    Started adding check function

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var number = "1234567890";
 var special = "!@#$%^&*()";
 var final = "";
-var pWord = ""
+var finArr = [];
+
+var pWord = "";
 // Write password to the #password input
 
 
@@ -28,30 +30,67 @@ var charNum = Number(prompt("Select number of charcters (between 8 and 128)"));
 
 var upSelect = confirm("Does the password require uppercase letters?");
 if  (upSelect === true) 
-{var final = final + uppercase};
-console.log(final);
+{var final = final + uppercase; finArr.push(uppercase);}
+
 
 var lowSelect = confirm("Does the password require lowercase letters?");
 if  (lowSelect === true) 
-{var final = final + lowercase};
-console.log(final);
+{var final = final + lowercase; finArr.push(lowercase);}
+
 
 var numSelect = confirm("Does the password require numbers?");
 if  (numSelect === true) 
-{var final = final + number};
-console.log(final);
+{var final = final + number; finArr.push(number);}
+
 
 var specSelect = confirm("Does the password require special characters?");
 if  (specSelect === true) 
-{var final = final + special};
-console.log(final);
+{var final = final + special; finArr.push(special);}
 
 
+console.log(finArr);
  
   function generatePassword() {
 var i  
-for ( i = 0; i < charNum; i++) {pWord += (final)[Math.floor(Math.random() * final.length)];};
+for ( i = 0; i < charNum; i++) {pWord += (final)[Math.floor(Math.random() * final.length)];}; 
 }
+
+
+
+
+
+  
+function findCommonElement() { 
+
+  var array1 = pWord.split("");
+
+  var array2 = array2(array2); 
+
+  function array2()
+  {return finArr[1].split("")};
+
+  var arrayLength = arrayLength(array2);
+  function arrayLength(array2) {return array2.length};
+
+      console.log(pWord);
+    
+    for(let i = 0; i < charNum; i++) { 
+          
+        
+        for(let j = 0; j < (arrayLength); j++) { 
+              
+           
+            if(array1[i] === array2[j]) { 
+              
+                 
+                console.log(true); 
+            } 
+        } 
+    } 
+      
+     console.log(false);  
+} 
+
 
 
 function writePassword() {
@@ -60,9 +99,18 @@ function writePassword() {
    passwordText.value = password;
    console.log(pWord);
 }
+
+
+
 generatePassword()
 
-writePassword()
+findCommonElement();
+
+writePassword();
+
+
+
+
 
 }
 
