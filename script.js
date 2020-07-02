eb769cb71730040a819cebb044559e118de915cb
Marc <mlanglois333@icloud.com>
2020-07-01 22:28:28 -0400

    scrapped check idea, rewrote whole thing using mathrandom and Fisher-Yates (almost working)

--- script.js
+++ script.js
@@ -1,120 +1,109 @@
-// Assignment Code
+
 
 var generateBtn = document.querySelector("#generate");
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var number = "1234567890";
-var special = "!@#$%^&*()";
-var final = "";
 var finArr = [];
-var tf = [];
 var pWord = "";
-// Write password to the #password input
 
 
-
-
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", selectInput);
 
 
 
-function selectInput(Characters) {
+function selectInput() {
   
 var charNum = Number(prompt("Select number of charcters (between 8 and 128)"));
 
 
-
-
-
 var upSelect = confirm("Does the password require uppercase letters?");
 if  (upSelect === true) 
-{var final = final + uppercase; finArr.push(uppercase);}
+{finArr.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");}
 
 
 var lowSelect = confirm("Does the password require lowercase letters?");
 if  (lowSelect === true) 
-{var final = final + lowercase; finArr.push(lowercase);}
+{finArr.push("abcdefghijklmnopqrstuvwxyz");}
 
 
 var numSelect = confirm("Does the password require numbers?");
 if  (numSelect === true) 
-{var final = final + number; finArr.push(number);}
+{finArr.push("1234567890");}
 
 
 var specSelect = confirm("Does the password require special characters?");
 if  (specSelect === true) 
-{var final = final + special; finArr.push(special);}
+{finArr.push("!@#$%^&*()");}
 
 
-console.log(finArr);
+
+var arrayLength = finArr.length;
+var passAmt = Math.floor(charNum / arrayLength);
+
+
  
-  function generatePassword() {
-
-for ( i = 0; i < charNum; i++) {pWord += (final)[Math.floor(Math.random() * final.length)];}; 
-}
-
-
-
-
-
-function checkPass() {
-
-  //defines index number of string to be checked
-var ind = 0
-//defines index to be checked//
-var indexNo = finArr[ind];
-//defines length of array (charset)//
-var arrInd = finArr.length;
-//turns password into array of characters//
-var array1 = pWord.split("");
-//splits charset into array//
-var array2 = indexNo.split("");
-//defintes length of charset//
-var arrayLength = array2.length;
-
-  // Loops through each selected character set //
-for (var h = 0; h < arrInd ; h++) {
-
-  console.log(ind);
-  console.log(indexNo);
-  console.log(array1);
-  console.log(array2);
-  console.log(arrayLength);
-  console.log(arrInd);
- 
-
-
-  // Loops through each item in index//
-
-    for(let i = 0; i < charNum; i++) { 
-          
-        
-        for(let j = 0; j < arrayLength; j++) { 
-              
-           
-            if(array1[i] === array2[j]) { 
-              tf = true}
-
-                 
-                } 
-        }  
-        
-        
-  } 
-
-}
-
-
+  function generatePassword() 
   
   
+  { for (i = 0; i < arrayLength; i++ ){
 
+
+var finArr1 = finArr[i];
+
+var stringLength = finArr1.length;
+
+    for ( j = 0; j < passAmt; j++) {pWord += (finArr1)[Math.floor(Math.random() * stringLength)];}; 
+  
+  }
+  }
+function addDiff() {  
+
+
+var pwdLength = pWord.length;
+var pwdDif = charNum - pwdLength;
+var zeroLength = finArr[0].length;
+
+
+  for ( i = 0; i < pwdDif; i++) {pWord += (finArr[0])[Math.floor(Math.random() * zeroLength)];}; 
+
+
+  }
+
+  
+
+ 
+    
+    
+    
+    function shuffle(array) {
+     
+     
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+     
+      while (0 !== currentIndex) {
+    
+        
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+     
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+    
+      var array = arr.join('');
+      return pWord;
+    }
+    
+ 
+  
+  
 
 
 
 function writePassword() {
-  var password = pWord;
+  var finalPword = pWord.join('');
+  var password = finalPword;
   var passwordText = document.querySelector("#password");
    passwordText.value = password;
    console.log(pWord);
@@ -122,11 +111,14 @@ function writePassword() {
 
 
 
-generatePassword()
+generatePassword();
+addDiff();
+   var arr = pWord.split("");
+   shuffle(arr);
+   console.log(pWord);
 
-checkPass();
-writePassword();
- console.log (tf)
+writePassword(finalPword);
+
 
 
 
